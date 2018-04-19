21c540d2de0789dc0609677e1d0f052010b60b8a
Falk Benke <benke.falk@gmail.com>
2018-04-19 14:20:59 +0000

    Add exception handling to tmdb API call

--- fantasy_cast/tmdb/views.py
+++ fantasy_cast/tmdb/views.py
@@ -17,29 +17,34 @@ class MovieSuggestions(APIView):
 
         search = tmdb.Search()
         query = request.query_params.get('query')
-        search.movie(query=query)
 
-        movies = []
+        if not query:
+            movies = []
 
-        for s in search.results:
+        else:
+            search.movie(query=query)
 
-            movies.append(
-                Movie(tmdb_id=s['id'],
-                      title=s['title'],
-                      release_date=s['release_date'],
-                      popularity=s['popularity'],
-                      overview=s['overview'],
-                      original_title=s['original_title'],
-                      original_language=s['original_language'],
-                      vote_count=s['vote_count'],
-                      vote_average=s['vote_average'],
-                      backdrop_path=s['backdrop_path'],
-                      poster_path=s['poster_path'],
-                      genres=s['genre_ids'],
-                      is_adult=s['adult'],
-                      is_video=s['video']
-                      )
-            )
+            movies = []
+
+            for s in search.results:
+
+                movies.append(
+                    Movie(tmdb_id=s['id'],
+                          title=s['title'],
+                          release_date=s['release_date'],
+                          popularity=s['popularity'],
+                          overview=s['overview'],
+                          original_title=s['original_title'],
+                          original_language=s['original_language'],
+                          vote_count=s['vote_count'],
+                          vote_average=s['vote_average'],
+                          backdrop_path=s['backdrop_path'],
+                          poster_path=s['poster_path'],
+                          genres=s['genre_ids'],
+                          is_adult=s['adult'],
+                          is_video=s['video']
+                          )
+                )
 
         serializer = MovieSerializer(movies, many=True)
 
