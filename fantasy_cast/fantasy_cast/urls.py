e3174082b54c4849033b4b9d7620201161e144af
Falk Benke <benke.falk@gmail.com>
2018-04-19 14:15:08 +0000

    Add standard API documentation

--- fantasy_cast/fantasy_cast/urls.py
+++ fantasy_cast/fantasy_cast/urls.py
@@ -1,13 +1,16 @@
+from rest_framework.documentation import include_docs_urls
+
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', RedirectView.as_view(url='docs')),
     url(r'^api/', include('rest_framework.urls')),
     url(r'^api/imdb/', include('imdb.urls')),
     url(r'^api/tmdb/', include('tmdb.urls')),
-    url(r'^api/remakes/', include('remake.urls'))
+    url(r'^api/remakes/', include('remake.urls')),
+    url(r'^docs/', include_docs_urls(title='Fantasy Cast'))
 ]
