7d3de86ad65dc5329417d470c8382c13d2920d8a
Falk Benke <benke.falk@gmail.com>
2018-06-04 13:00:22 +0000

    Protect remake API calls

--- fantasy_cast/remake/views.py
+++ fantasy_cast/remake/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from remake.models import Remake
 from remake.serializers import RemakeSerializer
 
 
 class RemakeList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Remake.objects.all()
     serializer_class = RemakeSerializer
 
 
 class RemakeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Remake.objects.all()
     serializer_class = RemakeSerializer
