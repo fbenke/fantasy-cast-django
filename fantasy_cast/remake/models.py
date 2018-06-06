fdc1cfa2d956577c25e59f12fa49321e3c55b1ad
Falk Benke <benke.falk@gmail.com>
2018-06-06 15:01:08 +0000

    Add movie and description to remake

--- fantasy_cast/remake/models.py
+++ fantasy_cast/remake/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from imdb.models import MovieTitle
 
 
 class Remake(models.Model):
 
     title = models.CharField(max_length=50)
+    description = models.TextField()
+    movie = models.ForeignKey(
+        MovieTitle, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
